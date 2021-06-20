7eac31a8de277d895971b38e808d66149c6454db
Ifrah Sajjad <67663463+isajjad1246@users.noreply.github.com>
2021-06-19 23:52:57 -0400

    tweaking search bar and button

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MovieInfo.css'
+
 function MovieInfo() {
+
+  const[query, setQuery]=useState("");
     return (
       <div className="movie-info">
         <form>
           <label htmlFor="queryInput"> Search Movie Here </label>
+          <input id="queryInput" 
+          type="text" 
+          value= {query}
+          onChange={e => setQuery(e.target.value)}
+          ></input> 
+          
         </form>
+        <button className="search-bar">Submit</button>
       </div>
     );
 } 
