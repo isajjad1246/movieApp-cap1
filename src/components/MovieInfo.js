10973d0cb5f5197c67dfbeb7c145e4e65f510cb0
Ifrah Sajjad <67663463+isajjad1246@users.noreply.github.com>
2021-06-20 00:37:59 -0400

    adding api link to movieinfo page

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -4,9 +4,13 @@ import './MovieInfo.css'
 function MovieInfo() {
 
   const[query, setQuery]=useState("");
+  function SubmitManage(e){
+    e.preventDefault();
+    const apiURL="http://www.omdbapi.com/?apikey=[process.env.REACT_APP_API_KEY]&s=${searchParam}&r=json`";
+  }
     return (
       <div className="movie-info">
-        <form>
+        <form onSubmit={SubmitManage}>
           <label htmlFor="queryInput"> Search Movie Here </label>
           <input id="queryInput" 
           type="text" 
